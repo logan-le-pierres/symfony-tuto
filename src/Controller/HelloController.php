28d24961df1b1f4744ab6bc295767a25b3e151f3
logan <loganlepierres1992@gmail.com>
2021-08-17 11:27:32 +0200

    Push sur demande de mymy

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,28 +6,30 @@ use App\Taxes\Calculator;
 use App\Taxes\Detector;
 use Cocur\Slugify\Slugify;
 use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Flex\Unpack\Result;
 use Twig\Environment;
 
-class HelloController {
+class HelloController extends AbstractController
+{
 
         // protected $calculator;
         // protected $logger;
         protected $twig;
 
 
-        public function __construct( LoggerInterface $logger, Calculator $calculator, Slugify $slugify, Detector $detector, Environment $twig )
-        {
-            // dump($detector->detect(101));
-            // dump($detector->detect(10));
-            // dump($slugify->slugify("Salut comment tu vas ?"));
-            // $this->calculator = $calculator;
-            // $logger->error("Mon message de log!");
-            $this->twig = $twig;
-        }
+        // public function __construct( LoggerInterface $logger, Calculator $calculator, Slugify $slugify, Detector $detector, Environment $twig )
+        // {
+        //     // dump($detector->detect(101));
+        //     // dump($detector->detect(10));
+        //     // dump($slugify->slugify("Salut comment tu vas ?"));
+        //     // $this->calculator = $calculator;
+        //     // $logger->error("Mon message de log!");
+        //     $this->twig = $twig;
+        // }
     
 
 
@@ -71,9 +73,11 @@ class HelloController {
         return $this->render('example.html.twig',['age'=>33]);
     }
 
-    protected function render(string $path, array $variable)
-    {
-        $html = $this->twig->render($path, $variable);
-            return new Response($html);
-    }
+    // protected function render(string $path, array $variable = [])
+    // {
+    //     $html = $this->twig->render($path, $variable);
+    //         return new Response($html);
+    // }
+
+
 }
