10a14d41e0ebe2c868989db6f77835ada5f167a9
logan <loganlepierres1992@gmail.com>
2021-08-20 18:37:18 +0200

    debut des autorisation/authentifications/voters

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,10 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
 
 class HomeController extends AbstractController
 {
@@ -16,7 +20,7 @@ class HomeController extends AbstractController
      */
     public function homepage(EntityManagerInterface $em)
     {
-        $productRepository = $em->getRepository(Product::class);
+        // $productRepository = $em->getRepository(Product::class);
 
         // 1/Permet de creer creer un nouveau produit (ici)
         // $product = new Product;
@@ -29,12 +33,69 @@ class HomeController extends AbstractController
         // $em->flush();
 
         // 2/ 
-        $product = $productRepository->find(2);
-        $product->setPrice(4500);
-        $em->flush();
-        dd($product);
+        // $product = $productRepository->find(2);
+        // $product->setPrice(4500);
+        // $em->flush();
+        // dd($product);
 
         return $this->render("home.html.twig");
-        
-    }
-}
+    }}
+    // /**
+    //  * @Route("/admin", name="admin_create")
+    //  */
+//     public function create(FormFactoryInterface $factory)
+//     {
+
+//         $builder = $factory->createBuilder();
+
+//         $builder->add('Identifiant_:', TextType::class, [
+//             'label' => 'Nom du produit',
+//             'attr' => ['class' => 'form-control', 'placeholder' => 'Tapez le nom du produit']
+//         ])
+//             ->add('Mot_de_passe_:', TextareaType::class, [
+//                 'label' => 'Description courte',
+//                 'attr' => [
+//                     'class' => 'form-control',
+//                     'placeholder' => 'Tapez une description assez courte mais parlante'
+//                 ]
+//             ]);
+
+//         // $options = [];
+
+//         // foreach ($categoryRepository->findAll() as $category) {
+//         //     $options[$category->getName()] = $category->getId();
+//         // }
+//         // dd($options);
+
+//         // $builder->add('Produit', ChoiceType::class, [
+//         //     'label' => 'Categorie',
+//         //     'attr' => ['class' => 'form-control'],
+//         //     'placeholder' => '--Choisir votre catégorie--',
+//         //     'choices' => [
+//         //         'Categorie 1' => 1,
+//         //         'Categorie 2' => 2
+
+//         //     ]
+//         // ]);
+
+
+//     //     $form = $builder->getForm();
+
+//     //     $formView = $form->createView();
+
+//     //     return $this->render("create.html.twig", [
+//     //         'formView' => $formView
+//     //     ]);
+//     // }
+
+//     // /** 
+//     // *@Route("/admin/{id}/edit", name"product_edit") 
+//     // */
+//     // public function edit($id, ProductRepository $productrepository){
+//     //         $product =$productrepository->find($id);
+//     //         return $this->render('create.html.twig',[
+//     //             'product' => $product
+//     //         ]);
+
+//     // }
+// }
